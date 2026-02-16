0c42812fd8b88c7415deabc2deabc4bc9ffccfb5
Shashank Shekhar Gupta <shashankgupta26122@gmail.com>
2026-02-16 14:17:40 +0530

    Time: 31 ms (38.59%) | Memory: 28.6 MB (96.52%) - LeetSync

--- 187-repeated-dna-sequences/repeated-dna-sequences.py
+++ 187-repeated-dna-sequences/repeated-dna-sequences.py
@@ -1,10 +1,22 @@
 class Solution:
     def findRepeatedDnaSequences(self, s: str) -> List[str]:
+        if len(s) <= 10:
+            return []
+            
         seen = set()
         res = set()
-        for i in range(len(s)-10+1):
-            dna = s[i:i+10]
-            if dna in seen:
-                res.add(dna)
-            seen.add(dna)
+        dna_map = {'A':0,'C':1,'G':2,'T':3}
+        k = 10
+        encode = 0
+        for i in range(k):
+            encode += (4 ** (k-i-1)) * dna_map[s[i]]
+        seen.add(encode)
+
+        for i in range(k,len(s)):
+            encode -= (4**9) * dna_map[s[i-k]] 
+            encode *= 4
+            encode += dna_map[s[i]]
+            if encode in seen:
+                res.add(s[i+1-k:i+1])
+            seen.add(encode)
         return list(res)
