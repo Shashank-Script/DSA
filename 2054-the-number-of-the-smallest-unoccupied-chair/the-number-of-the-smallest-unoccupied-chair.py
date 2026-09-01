e0ba8208705c5582e0467b953a7711d33abe5b9e
Shashank Shekhar Gupta <shashankgupta26122@gmail.com>
2026-09-01 23:47:17 +0530

    Time: 71 ms (52.92%) | Memory: 23.9 MB (86.13%) - LeetSync

--- 2054-the-number-of-the-smallest-unoccupied-chair/the-number-of-the-smallest-unoccupied-chair.py
+++ 2054-the-number-of-the-smallest-unoccupied-chair/the-number-of-the-smallest-unoccupied-chair.py
@@ -1,21 +1,30 @@
 class Solution:
     def smallestChair(self, times: List[List[int]], targetFriend: int) -> int:
-        for i in range(len(times)):
-            times[i].append(i)
+        targetFriArrive = times[targetFriend][0]
 
         times.sort(key = lambda x : x[0])
-        chairs = []
-        record = {}
-        for arrive,leave,fri in times:
-            for i in range(len(chairs)):
-                if chairs[i] <= arrive:
-                    chairs[i] = leave
-                    record[fri] = i
-                    break
-            else:
-                chairs.append(leave)
-                record[fri] = len(chairs)-1
+        
+        freeChairs = []
+        occupChairs = []
 
-        return record[targetFriend]
+        for i in range(len(times)):
+            heapq.heappush(freeChairs,i)
+        
+        for arrive,leave in times:
+            while occupChairs:
+                freeAt,chair = occupChairs[0]
+                if arrive >= freeAt:
+                    chairs = heapq.heappop(occupChairs)
+                    heapq.heappush(freeChairs, chairs[1])
+                else:
+                    break
+            
+            freeChair = heapq.heappop(freeChairs)
+            heapq.heappush(occupChairs,(leave,freeChair))
+            if arrive == targetFriArrive:
+                return freeChair
+                    
+
+            
 
             
