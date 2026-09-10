8b75c0f952702c18b0f19100315c751d29ebabdf
Shashank Shekhar Gupta <shashankgupta26122@gmail.com>
2026-09-10 22:53:14 +0530

    Time: 41 ms (58.05%) | Memory: 24.6 MB (16.88%) - LeetSync

--- 436-find-right-interval/find-right-interval.py
+++ 436-find-right-interval/find-right-interval.py
@@ -1,23 +1,23 @@
 class Solution:
     def findRightInterval(self, intervals: List[List[int]]) -> List[int]:
         n = len(intervals)
-        res = n * [0]
+        res = n * [-1]
+
+        min_start = []
+        min_end = []
 
         for i in range(n):
-            intervals[i].append(i)
+            heapq.heappush(min_start,(intervals[i][0],i))
+            heapq.heappush(min_end,(intervals[i][1],i))
+            
+        while min_start and min_end:
+            start,start_idx = min_start[0]
+            end,end_idx = min_end[0]
 
-        intervals.sort(key=lambda x : x[0])
-
-        for i in range(n):
-            l,r = i,n-1
-            idx = -1
-            while l <= r:
-                mid = (l+r)//2
-                if intervals[mid][0] >= intervals[i][1]:
-                    idx = intervals[mid][2]
-                    r = mid - 1
-                else:
-                    l = mid + 1
-
-            res[intervals[i][2]] = idx
+            if start >= end:
+                heapq.heappop(min_end)
+                res[end_idx] = start_idx
+            else:
+                heapq.heappop(min_start)
+        
         return res
